6f127617e9cc5d01b8439ecd4e146025886dc842
pikapika2 <yossii0108@gmail.com>
2021-11-12 23:08:27 +0900

    add connect api
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import React, { FC } from 'react'
+
+export default function Home(props) {
   return (
     <div>
       <h1>template</h1>
+      {props.posts.result.map((item) => {
+        return (
+          <label>
+            <input type="checkbox" key={item.prefCode} />
+            {item.prefName}
+          </label>
+        )
+      })}
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const url = encodeURI(
+    'https://opendata.resas-portal.go.jp/api/v1/prefectures'
+  )
+  const key = {
+    headers: {'x-api-key': process.env.API_KEY}
+  }
+  const posts = await fetch(url, key).then(res => res.json())
+  console.log(posts.result)
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
