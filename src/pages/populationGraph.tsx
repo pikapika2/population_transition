78f32119692944098c3d71f1797255ab26866052
pikapika2 <yossii0108@gmail.com>
2021-11-17 23:45:16 +0900

    add dummy data from RESAS-API
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/populationGraph.tsx
+++ src/pages/populationGraph.tsx
@@ -1,27 +1,57 @@
 import React from "react";
+import useSWR from 'swr';
 import Highcharts from "highcharts";
 import HighchartsExporting from 'highcharts/modules/exporting'
 import HighchartsReact from "highcharts-react-official";
 
+const keys: any = {
+      headers: { 'x-api-key': process.env.NEXT_PUBLIC_API_KEY },
+    }
+
+const fetcher = (url: any) => fetch(url, keys).then((res) => res.json())
+
 const PopulationGraph: React.FunctionComponent = () => {
   if (typeof Highcharts === 'object') {
-    HighchartsExporting(Highcharts);
+    HighchartsExporting(Highcharts)
+  }
+
+  const res: any = useSWR('https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=11362&prefCode=11', fetcher)
+  console.log(res)
+  if(! res) {
+  	return (
+      <div>
+        loading
+      </div>
+  	)
+  }
+  /*if(error){
+  	return (
+      <div>
+        fail to load
+      </div>
+  	)
+  }*/
+
+  let data = []
+
+  for (let pd of res.data.result.data[0].data) {
+  	data.push(pd.value)
   }
 
   const options = {
-  title: {
-    text: 'My chart'
-  },
-  series: [{
-    data: [1, 2, 3]
-  }]
-}
+    title: {
+      text: 'My chart'
+    },
+    series: [{
+      data: data
+    }]
+  }
 
   return (
     <div>
       <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
-  );
-};
+  )
+}
 
 export default PopulationGraph
