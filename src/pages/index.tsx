26d54a5dd57727bda533e1b7aed44afad525aff0
pikapika2 <yossii0108@gmail.com>
2021-11-18 21:58:27 +0900

    link checkbox
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,28 @@ import PopulationGraph from './populationGraph'
 
 export default function Home(props: any) {
   const [population, setPopulation] = useState<{
-    result: {
-      prefName: string,
-      data: {
-        year: number,
-        value: number,
-      }
-    }
+    prefName: string,
+    data: {
+      year: number,
+      value: number,
+    }[]
   }[]>([])
 
-  const clickCheckbox = () => {
-    console.log('ssss')
+  //const fetcher = (url: any) => fetch(url, client_keys).then((res) => res.json())
+
+  const clickCheckbox = async (event: any) => {
+    let existPopulation = population.slice()
+    console.log(event.checked)
+    const clientKeys: any = {
+      headers: { 'x-api-key': process.env.NEXT_PUBLIC_API_KEY },
+    }
+    const url: any = 'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=' + event.id
+    const posts = await fetch(url, clientKeys).then((res) => res.json())
+    existPopulation.push({
+      prefName: event.name,
+      data: posts.result.data[0].data
+    })
+    setPopulation(existPopulation)
     return
   }
 
@@ -24,12 +35,12 @@ export default function Home(props: any) {
       {props.posts.result.map((item: any) => {
         return (
           <label key={item.prefCode}>
-            <input type="checkbox" key={item.prefCode} id={item.prefCode} onChange={clickCheckbox}/>
+            <input type="checkbox" key={item.prefCode} id={item.prefCode} name={item.prefName} onChange={(e: React.ChangeEvent<HTMLInputElement>): Promise<void> => clickCheckbox(e.target)}/>
             {item.prefName}
           </label>
         )
       })}
-      <PopulationGraph />
+      <PopulationGraph populationData={population}/>
     </main>
   )
 }
--- src/pages/populationGraph.tsx
+++ src/pages/populationGraph.tsx
@@ -1,44 +1,44 @@
 import React from 'react'
-import useSWR from 'swr'
-import fetch from 'unfetch'
 import Highcharts from 'highcharts'
 import HighchartsExporting from 'highcharts/modules/exporting'
 import HighchartsReact from 'highcharts-react-official'
 
-const keys: any = {
-  headers: { 'x-api-key': process.env.NEXT_PUBLIC_API_KEY },
+type Props = {
+  populationData: {
+    prefName: string,
+    data: {
+      year: number,
+      value: number,
+    }[]
+  }[]
 }
 
-const fetcher = (url: any) => fetch(url, keys).then((res) => res.json())
-
-const PopulationGraph: React.FunctionComponent = () => {
+const PopulationGraph: React.FunctionComponent<Props> = ({populationData}) => {
   if (typeof Highcharts === 'object') {
     HighchartsExporting(Highcharts)
   }
-
-  const res: any = useSWR(
-    'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=33',
-    fetcher
-  )
-  
-  if (!res.data) {
-    return <div>loading</div>
-  }
-  /*if(error){
-  	return (
-      <div>
-        fail to load
-      </div>
-  	)
-  }*/
-
-  let data = []
+  let series = []
   let year = []
 
-  for (let pd of res.data.result.data[0].data) {
-    data.push(pd.value)
-    year.push(pd.year)
+  for (let p of populationData){
+    let data = []
+
+    for (let pd of p.data) {
+      data.push(pd.value)
+      year.push(pd.year)
+    }
+    series.push({
+      name: p.prefName,
+      data: data,
+    })
   }
+  if(series.length === 0){
+    return(
+      <div>
+      not data
+      </div>)
+  }
+  console.log(series)
 
   const options = {
     title: {
@@ -55,11 +55,7 @@ const PopulationGraph: React.FunctionComponent = () => {
       },
       categories: year,
     },
-    series: [
-      {
-        data: data,
-      },
-    ],
+    series: series,
   }
 
   return (
