4c05715966ab1b5216fbb7014af1c0093a31a9a2
pikapika2 <yossii0108@gmail.com>
2021-11-19 03:27:07 +0900

    resolve format
    Signed-off-by: pikapika2 <yossii0108@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,8 @@ export default function Home(props: any) {
       }
       const url: any = encodeURI(
         'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=' +
-        event.id)
+          event.id
+      )
       const posts = await fetch(url, clientKeys).then((res) => res.json())
       existPopulation.push({
         prefName: event.name,
